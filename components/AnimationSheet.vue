e94aed4fe7392c8ac9b49a582135a6bf91bfd2f9
Worraanong Chanchaijak <c.worraanong@gmail.com>
2024-10-09 04:59:34 +0700

    moodeng component

--- components/AnimationSheet.vue
+++ components/AnimationSheet.vue
@@ -1,32 +1,25 @@
-<script>
-import '~/assets/css/animated.css'
-</script>
-
 <template>
     <div>
         <p>Sprite by <a href="https://limpck.itch.io/moodeng">LimpCK</a></p>
-        <img src="/moodeng.png">
+        <img src="~/assets/img/moodeng.png">
         <div class="row">
             <div class="col">
-
                 <h2>① Animation</h2>
-                <div class="base break">Break</div>
-                <div class="base charge">Charge</div>
-                <div class="base float">Float</div>
-                <div class="base stand">Stand</div>
-                <div id="run" class="base stand">Run</div>
+                <Moodeng pose="break"/>
+                <Moodeng pose="charge"/>
+                <Moodeng pose="float"/>
+                <Moodeng/>
+                <Moodeng pose="run"/>
             </div>
             <div class="col">
                 <h3>Hover to ...</h3>
-                Dash
-                <div id="dash" class="base stand"></div>
-                <div id="flinch" class="base break">Flinch</div>
-                <div id="crouch" class="base stand">Crouch</div>
-                Turn
-                <div id="turn" class="base stand"></div>
-                <div id="jump" class="base stand">Jump</div>
+                <Moodeng pose="dash"/>
+                <Moodeng pose="flinch"/>
+                <Moodeng pose="crouch"/>
+                <Moodeng pose="turn"/>
+                <Moodeng pose="jump"/>
+
             </div>
         </div>
-
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["nuxt-lodash"],
+  lodash: {
+    prefix: "_",
+    upperAfterPrefix: true,
+  },
+});
