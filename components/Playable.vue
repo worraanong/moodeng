78ec33bfae3c92e3bbc6ef7fc82041f3575a1e8b
Worraanong Chanchaijak <c.worraanong@gmail.com>
2024-10-09 08:56:14 +0700

    clickable collision

---
+++ components/Playable.vue
@@ -0,0 +1,25 @@
+<script setup>
+import '~/assets/css/layer.css'
+const newPose = ref('stand')
+const randomPose = () => {
+    newPose.value = 'flinch-auto'
+    _Delay(() => { newPose.value = 'stand' }, 800)
+}
+
+</script>
+
+<style>
+.flinch-auto {
+    animation: aniFlinch 0.2s steps(2) 3;
+}
+.layer-collision {
+  cursor: pointer;
+
+}
+</style>
+<template>
+    <div class="layer-invisible layer-sprite">
+        <div class="layer-invisible layer-collision" @click="randomPose()"></div>
+        <Moodeng :pose="newPose" />
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
-<script>
+<script setup>
 import '~/assets/css/main.css'
+
 </script>
 <template>
     <h1>Moo Deng</h1>
     <p>Hacktoberfest 2024 mini project</p>
 
     <div id="play-area">
-        เดี๋ยวมาเขียนต่อ ง่วง
-        <Moodeng />
+        เอ้ย อย่ามาจิ้มนะ!
+        <Playable />
     </div>
 
     <AnimationSheet></AnimationSheet>
