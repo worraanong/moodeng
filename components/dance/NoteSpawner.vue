cee06457ccd7dedf7ab1277c28d9877edeff0e2d
Worraanong Chanchaijak <c.worraanong@gmail.com>
2024-10-15 07:58:33 +0700

    well it move, just incorrectly

--- components/dance/NoteSpawner.vue
+++ components/dance/NoteSpawner.vue
@@ -10,40 +10,53 @@ class Note {
     dir: string
     left: number
 
-    constructor(dir = 'up', left = 0 ){
+    constructor(dir = 'up', left = 0) {
         this.dir = dir
         this.left = left
     }
 }
 
 const config = {
-    speed: 4,
-    totalNotes: 28,
+    speed: 10,
+    totalNotes: 8,
     spawnFrequency: 500,
-    duration: 15 * 1000,
+    duration: 25 * 1000,
 } as const
 const styles = reactive({
     left: '0px'
 })
 
+const time = ref(0)
+
+const dash = () => {
+    const id = setInterval(move, 20);
+    _Delay(() => {
+        clearInterval(id)
+    }, 1000)
+}
+const lapse = () => {
+    time.value += 1
+}
+
 const start = () => {
     ready.value = false
-    const id = setInterval(spawn,config.spawnFrequency );
+    time.value = 0
+    const id = setInterval(spawn, config.spawnFrequency);
+    const id2 = setInterval(lapse, 2);
     assignNotes(config.totalNotes)
     _Delay(() => {
         clearInterval(id)
+        clearInterval(id2)
         ready.value = true
         notesOnScreen.value = []
     }, config.duration)
 }
 
 const spawn = () => {
-    if (notes.value.length > 1) {
+    if (notes.value.length > 0) {
         const note = notes.value.pop() as string
         const n = new Note(note);
         notesOnScreen.value.push(n)
-
-        dash()
     }
 }
 
@@ -75,28 +88,56 @@ const move = () => {
     }
 }
 
-const dash = () => {
-    const id = setInterval(move, 20);
-    _Delay(() => {
-        clearInterval(id)
-    }, 1000)
-}
+
 
 const getLeftPos = () => {
     return _ToInteger(_TrimEnd(styles.left, 'px'))
 }
 
-const computeLeft = () => {
-    const x = _Random(2, 250)
-    return `${x}px`
+const computeLeft = (dir, i) => {
+    const x = time.value//_Random(2, 250)
+    //console.log("--- "+i)
+
+    //x - (i*25)
+
+    // 25 (500/20)
+
+    const m = 250//25
+    switch (dir) {
+        case "left":
+        case "down":
+            return `${x - (i * m)}px`
+        case "right":
+        case "up":
+            return `-${x - (i * m)}px`
+    }
+
+    return `10px`
+}
+
+const updatedPos = (dir, i) => {
+
+    switch (dir) {
+        case "left":
+            return { left: computeLeft(dir, i), top: '0px' }
+        case "right":
+            return { left: computeLeft(dir, i), top: '0px' }
+        case "up":
+            return { left: '0px', top: computeLeft(dir, i) }
+        case "down":
+            return { left: '0px', top: computeLeft(dir, i) }
+    }
+
+
+    return { left: computeLeft(dir, i), top: '10px' }
 }
 
 </script>
 <template>
+    {{ time }}
     <button v-show="ready" class="tiny btn-start" role="button" @click="start()"></button>
-    <!-- v-show="!ready"  -->
-    <div class="where" v-for="n in notesOnScreen" :key="n">
-            <DanceArrow :dir="n.dir" :styles="[{left: computeLeft()}]" />
+    <div class="where" v-for="(n, i) in notesOnScreen" :key="n">
+        <DanceArrow :dir="n.dir" :styles="[updatedPos(n.dir, i)]" />
     </div>
 </template>
 
@@ -109,9 +150,10 @@ const computeLeft = () => {
 .test {
     position: absolute;
 }
+
 .where {
-    background-color: blueviolet;
+    /* background-color: blueviolet;
     width: 20px;
-    height: 30px;
+    height: 30px; */
 }
 </style>
