c064ac2fb8ecadb1446dbb56e199d478bbc46cf7
Worraanong Chanchaijak <c.worraanong@gmail.com>
2024-10-21 22:09:13 +0700

    temporary fix

--- server/api/dumdum.get.ts
+++ server/api/dumdum.get.ts
@@ -4,7 +4,7 @@ import * as readline from "node:readline";
 import { resolvePath } from "nuxt/kit";
 
 export default defineEventHandler(async (event) => {
-  const data: string[] = [];
+  let data: string[] = [];
   //const filePath = resolvePath("/dumdum.txt")
 
   //const filePath = path.resolve(__dirname,"public", "dumdum.txt");
@@ -16,6 +16,8 @@ export default defineEventHandler(async (event) => {
   for await (const line of lineReader) {
     data.push(line);
   }
+
+  data = ['aa','','','','','','dd','','','','','','w','','','','w','','','','a','','d','','a','','aa','','','','d','','dd','','','','s','','s','','s','s','d','a','d','a','w','']
   return data;
   //   const data = await fs.promises.readFile(filePath, "utf-8");
 });
