53da257074d7bc2910544edc3abe0cf3a3ad80bb
Worraanong Chanchaijak <c.worraanong@gmail.com>
2024-10-22 14:07:02 +0700

    maybe the import?

--- server/api/dumdum.get.ts
+++ server/api/dumdum.get.ts
@@ -1,23 +1,29 @@
 import * as path from "node:path";
 import * as fs from "node:fs";
 import * as readline from "node:readline";
-import { resolvePath } from "nuxt/kit";
+import * as process from "node:process"
 
 export default defineEventHandler(async (event) => {
   let data: string[] = [];
+
+  try {
   //const filePath = resolvePath("/dumdum.txt")
 
   //const filePath = path.resolve(__dirname,"public", "dumdum.txt");
   //console.log(filePath)
-  // const filePath = path.join(process.cwd(), "public", "dumdum.txt");
-  // const lineReader = readline.createInterface({
-  //   input: fs.createReadStream(filePath),
-  // });
-  // for await (const line of lineReader) {
-  //   data.push(line);
-  // }
-
+   const filePath = path.join(process.cwd(), "public", "dumdum.txt");
+   console.log(`File path: ${filePath}`)
+   const lineReader = readline.createInterface({
+    input: fs.createReadStream(filePath),
+  });
+  for await (const line of lineReader) {
+    data.push(line);
+  }
+  }
+  catch (err) {
+    console.log(`Read file error: ${err}`)
   data = ['aa','','','','','','dd','','','','','','w','','','','w','','','','a','','d','','a','','aa','','','','d','','dd','','','','s','','s','','s','s','d','a','d','a','w','']
+  }
   return data;
   //   const data = await fs.promises.readFile(filePath, "utf-8");
 });
